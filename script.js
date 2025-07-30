722c596701dc391e2ff2990444a1ef3e91aa7937
authdev2 <flaviogomes05.work@gmail.com>
2025-07-30 02:15:25 +0100

    Melhorado um pouco do design (cores, paddings etc), Adicionado menu de pesquisa para mobile, adicionado responsividade completa para qualquer tipo de ecras

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let view = document.querySelector(".view");
 let hiddenLinks = document.querySelector(".hidden-links");
-
 let imgArrows = document.querySelector(".imgarrows");
+
 view.addEventListener("click", (e) => {
   e.preventDefault();
   hiddenLinks.classList.toggle("show");
@@ -11,4 +11,10 @@ view.addEventListener("click", (e) => {
   } else {
     imgArrows.src = "./images/icon-arrow-down.svg";
   }
+});
+
+let menuMobileBtn = document.querySelector(".menu-mobile-btn");
+let ativarMenuLinks = document.querySelector(".links-mobile");
+menuMobileBtn.addEventListener("click", () => {
+  ativarMenuLinks.classList.toggle("flex-ativar");
 });
